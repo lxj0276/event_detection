2809d6ecc33a04faca6e90532e342b9566196d91
Your Name <you@example.com>
2016-04-14 18:42:58 +0800

    add

--- eventdetection.h
+++ eventdetection.h
@@ -2,11 +2,13 @@
 #define _EVENTDETECTION_H
 #include <math.h>
 #include <algorithm>
+#include <queue>
 #include "../commom/func.h"
 #include "../wordseg/seg.h"
 #include "../textanalysis/keywords.h"
 #define KEYWORDSNUM 4
-typedef std::map<std::string, std::map<std::string,int> >daily_dict;
+#define SAVEDATE 7
+typedef std::map<std::string, std::queue<int> >daily_dict;
 namespace event{
 	//统计算法类
 	class  EventDetc{
@@ -14,14 +16,15 @@ namespace event{
 		commom::Func f;
 		seg::Wordseg mseg;
 		text::Keywords keywd;
+		daily_dict worddict;
 		//std::map<std::string, float>idfdict;
 	public:
 		EventDetc();
 		~EventDetc();
 		void Init(const char* path);
-		void WordCount(const char* filein, const char* fileout);
+		//void WordCount(const char* filein, const char* fileout);
 		void WordCountDaily(const char* filein, const char* fileout);
-		void SentenceCount(const char* filein, const char* fileout);
+		//void SentenceCount(const char* filein, const char* fileout);
 	};
 }
 #endif
